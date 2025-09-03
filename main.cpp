cf58cf7cd408f99698583fb652bb4e518cc1bfeb
TituseonCETI <a24310334@live.ceti.mx>
2025-09-02 23:00:13 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,1 @@
-#include <Foco.hpp>
-#include <iostream>
-using namespace std;
-int main()
-{
-  cout << "Hola Mundo" << endl;
-  Foco foco;
-  foco.Encender()
-    cout << "EstadoFoco: " << foco.LeerEstado() << endl;
-  retunr 0;
-}
+
