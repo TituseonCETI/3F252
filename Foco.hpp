751ae0715cf18032732fff7ca641432c7e7b17d0
TituseonCETI <a24310334@live.ceti.mx>
2025-09-02 23:12:41 -0600

    Foco.hpp

--- Foco.hpp
+++ Foco.hpp
@@ -1,1 +1,12 @@
-
+typedef bool EstadoFoco;
+class Foco
+{
+private:
+   EstadoFoco estado;
+public:
+    Foco() {}
+    Foco() {}
+    void Encender(){estado=true;}
+    void Apagar(){estado=false;}
+    EstadoFisico LeerEstado(){return estado;}
+};
