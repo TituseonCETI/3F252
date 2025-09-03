9f3be2ef02b6fde705417a628f2661ab101ad120
TituseonCETI <a24310334@live.ceti.mx>
2025-09-02 22:22:51 -0600

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <Foco.hpp>
+#include <iostream>
+using namespace std;
+int main()
+{
+  cout << "Hola Mundo" << endl;
+  Foco foco;
+  foco.Encender()
+    cout << "EstadoFoco: " << foco.LeerEstado() << endl;
+  retunr 0;
+}
